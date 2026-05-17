cffa1e13db8f1b14b10045e5cacb4082a244f7b3
Kirill Volychev <volychevk@gmail.com>
2026-05-18 00:23:54 +0300

    fix: improve path logic

--- src/main/kotlin/ru/volychev/subscription/v1/config/ApiPaths.kt
+++ src/main/kotlin/ru/volychev/subscription/v1/config/ApiPaths.kt
@@ -2,7 +2,7 @@ package ru.volychev.subscription.v1.config
 
 object ApiPaths {
     private const val VERSION = "v1"
-    private const val BASE_PATH = "\${app.api.prefix}/$VERSION"
+    private const val BASE_PATH = "/\${app.api.prefix}/$VERSION"
 
     const val USERS = "$BASE_PATH/users"
     const val SERVICES = "$BASE_PATH/services"
